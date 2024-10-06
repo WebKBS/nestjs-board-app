9149687750607fb37605ceed83bd466989b1913d
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-06 18:56:17 +0900

    add: 커스텀 파이프 조건 추가하기

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,9 +1,23 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata): any {
-    console.log('value', value);
-    console.log('metadata', metadata);
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
+    }
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.StatusOptions.indexOf(status);
+    return idx !== -1;
   }
 }
